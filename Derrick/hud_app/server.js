ca809eb08faf0b758b6f082de9cff1ff50e0d2a4
Communications <communications@Communicationss-MacBook-2.local>
2018-04-07 17:38:24 -0400

    Created base exress app

---
+++ Derrick/hud_app/server.js
@@ -0,0 +1,14 @@
+import flask
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def index():
+  '''Displays the index page available at /
+  '''
+  return flask.render_template('index.html')
+
+if __name__ == '__main__':
+  app.debug=True
+  app.run()
+
